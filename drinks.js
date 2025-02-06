b29c5b3d308ea4c360dbbffa6f9f8da03ec89344
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-02-06 12:47:10 +0100

    audio

--- drinks.js
+++ drinks.js
@@ -4,18 +4,21 @@ const drinkThree = document.getElementById("sider");
 const nameInputEl = document.getElementById("name");
 const sendButton = document.getElementById("send");
 drinkOne.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
     drinkTwo.checked = false;
     drinkThree.checked = false;
     nameInputEl.disabled = false;
     nameInputEl.required = true;
 })
 drinkTwo.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
     drinkOne.checked = false;
     drinkThree.checked = false;
     nameInputEl.disabled = false;
     nameInputEl.required = true
 })
 drinkThree.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
     drinkOne.checked = false;
     drinkTwo.checked = false;
     nameInputEl.disabled = false;
@@ -26,17 +29,29 @@ drinkThree.addEventListener("click", () => {
 
 
 sendButton.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
     const drinkA = drinkOne.value;
     const drinkB = drinkTwo.value;
     const drinkC = drinkThree.value;
     const name = nameInputEl.value;
+    
     if (drinkOne.checked) {
+        
         alert(`thank you ${name}. You have Chosen ${drinkA}.`);
+        
     }
     if (drinkTwo.checked) {
+        
         alert(`Thank you ${name}. You have chosen ${drinkB}`);
+        
     }
     if (drinkThree.checked) {
         alert(`Thank you ${name}. You have chosen ${drinkC}.`);
+        
     }
 })
+function playSound(file) {
+let audio = new Audio();
+audio.src = file;
+audio.play();
+}
