6be17a55aaa859f60a941260108f256f80edfcb8
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-03-11 11:21:00 +0100

    nameinput

--- animals.js
+++ animals.js
@@ -1,30 +1,35 @@
 const animal1 = document.getElementById("hund");
 const animal2 = document.getElementById("katt");
 const animal3 = document.getElementById("tiger");
+const nameInputEl = document.getElementById("nameInput");
 const button = document.getElementById("sendButton");
 animal1.addEventListener("click", () => {
 animal2.checked = false;
-animal3.checked = false;    
+animal3.checked = false;
+nameInputEl.required = true;
 })
 animal2.addEventListener("click", () => {
 animal1.checked = false;
 animal3.checked = false;
+nameInputEl.required = true;
 })
 animal3.addEventListener("click", () => {
 animal1.checked = false;
 animal2.checked = false;
+nameInputEl.required = true;
 })
 button.addEventListener("click", () => {
 const animalA = animal1.value;
 const animalB = animal2.value;
 const animalC = animal3.value;
+const name = nameInputEl.value;
 if(animal1.checked) {
-alert(`Thank you, you have chosen ${animalA}.`);
+alert(`Thank you ${name}. You have chosen ${animalA}.`);
 }
 if(animal2.checked) {
-alert(`Thank you, you have chosen ${animalB}.`);
+alert(`Thank you ${name}. You have chosen ${animalB}.`);
 }
 if(animal3.checked) {
-alert(`Thank you, you have chosen ${animalC}.`);
+alert(`Thank you ${name}. You have chosen ${animalC}.`);
 }
 })
