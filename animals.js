83bcd9a0c48befe8d4186a4dc0540afb61a8fad7
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-03-12 09:05:07 +0100

    select all

--- animals.js
+++ animals.js
@@ -1,8 +1,15 @@
+const selectAllAnimals = document.getElementById("allChecked");
 const animal1 = document.getElementById("hund");
 const animal2 = document.getElementById("katt");
 const animal3 = document.getElementById("tiger");
 const nameInputEl = document.getElementById("nameInput");
 const button = document.getElementById("sendButton");
+selectAllAnimals.addEventListener("click", () => {
+animal1.checked = true;
+animal2.checked = true;
+animal3.checked = true;
+nameInputEl.required = true;
+})
 animal1.addEventListener("click", () => {
 animal2.checked = false;
 animal3.checked = false;
@@ -23,6 +30,9 @@ const animalA = animal1.value;
 const animalB = animal2.value;
 const animalC = animal3.value;
 const name = nameInputEl.value;
+if(selectAllAnimals.checked) {
+alert(`Thank you ${name}. You have chosen ${animalA}, ${animalB} och ${animalC}.`);
+}
 if(animal1.checked) {
 alert(`Thank you ${name}. You have chosen ${animalA}.`);
 }
