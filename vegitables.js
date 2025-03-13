5a340fc42e6bd6adea6eae6b7847523694d28f21
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-03-13 09:03:22 +0100

    select all

--- vegitables.js
+++ vegitables.js
@@ -1,8 +1,17 @@
 const vegitableOne = document.getElementById("morot");
+const selectAllVegitables = document.getElementById("allChecked");
 const vegitableTwo = document.getElementById("gurka");
 const vegitableThree = document.getElementById("paprika");
 const nameInput = document.getElementById("name");
 const button = document.getElementById("addButton");
+selectAllVegitables.addEventListener("click", () => {
+vegitableOne.checked = true;
+vegitableTwo.checked = true;
+vegitableThree.checked = true;
+nameInput.disabled = false;
+nameInput.required = true;
+button.disabled = false;
+})
 vegitableOne.addEventListener("click", () => {
     nameInput.disabled = false;
 nameInput.required = true;
@@ -29,6 +38,9 @@ button.addEventListener("click", () => {
     const vegitableB = vegitableTwo.value;
     const vegitableC = vegitableThree.value;
 const name = nameInput.value;
+if(selectAllVegitables.checked) {
+alert(`Thank you ${name}. You have chosen ${vegitableA}, ${vegitableB} och ${vegitableC}.`);
+}
     if (vegitableOne.checked) {
         alert(`tack ${name}. Du har valt grönsaken ${vegitableA}`);
     }
