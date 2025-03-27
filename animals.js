709ec17b1d27286bb8989c9b0769cc72e621ccd5
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-03-27 10:29:25 +0100

    audio

--- animals.js
+++ animals.js
@@ -6,22 +6,26 @@ const animal3 = document.getElementById("tiger");
 const nameInputEl = document.getElementById("nameInput");
 const button = document.getElementById("sendButton");
 selectAllAnimals.addEventListener("click", () => {
+playSound("audios/ping01-104055.mp3");
 animal1.checked = true;
 animal2.checked = true;
 animal3.checked = true;
 nameInputEl.required = true;
 })
 animal1.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
 animal2.checked = false;
 animal3.checked = false;
 nameInputEl.required = true;
 })
 animal2.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
 animal1.checked = false;
 animal3.checked = false;
 nameInputEl.required = true;
 })
 animal3.addEventListener("click", () => {
+    playSound("audios/ping01-104055.mp3");
 animal1.checked = false;
 animal2.checked = false;
 nameInputEl.required = true;
@@ -32,6 +36,7 @@ const animalB = animal2.value;
 const animalC = animal3.value;
 const name = nameInputEl.value;
 if(selectAllAnimals.checked) {
+
 alert(`Thank you ${name}. You have chosen ${animalA}, ${animalB} och ${animalC}.`);
 
 }
@@ -45,3 +50,8 @@ if(animal3.checked) {
 alert(`Thank you ${name}. You have chosen ${animalC}.`);
 }
 })
+function playSound(file) {
+let audio = new Audio();
+audio.src = file;
+audio.play();
+}
